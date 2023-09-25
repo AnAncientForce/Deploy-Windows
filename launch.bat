319e1ce5b5844980af4581af8d8258b1ce8d0163
AnAncientForce <zflyway45@protonmail.com>
2023-09-25 16:05:14 +0100

    c

--- launch.bat
+++ launch.bat
@@ -1,2 +1,12 @@
 @echo off
+echo Running as administrator...
+>nul 2>&1 "%SYSTEMROOT%\System32\cacls.exe" "%SYSTEMROOT%\System32\config\system" || (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+)
 python "%~dp0main.py"
+goto :EOF
+
+:UACPrompt
+powershell.exe -Command "Start-Process -FilePath '%0' -ArgumentList '%~dp0main.py' -Verb RunAs"
+exit /B
