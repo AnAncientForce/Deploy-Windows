9cf04775d13a4fa267b1eb935ee3cb067f246d9f
AnAncientForce <zflyway45@protonmail.com>
2023-09-25 13:20:28 +0100

    c

--- setup.bat
+++ setup.bat
@@ -11,12 +11,8 @@ set "WORKING_DIRECTORY=%SCRIPT_DIR%"
 rem Navigate to the working directory
 cd /d "%WORKING_DIRECTORY%"
 
-rem Clone or update the GitHub repository
-git clone https://github.com/AnAncientForce/Deploy-Windows.git 2>nul || (
-    echo Repository already exists, updating...
-    cd "Deploy-Windows"
-    git pull
-)
+rem Update the current folder from the GitHub repository
+git pull
 
 echo Done!
 
